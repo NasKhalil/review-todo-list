12ea020674f5a5fadb72e0157d962652b7e2a8c3
Khalil <khalilnasri92@gmail.com>
2021-12-08 15:01:47 +0100

    test function addTodo

---
+++ src/__mocks__/addTodo.test.js
@@ -0,0 +1,11 @@
+import addTodo from '../addTodo'
+
+describe('test add function', () => {
+  const arr = [];
+  const todoStr = 'test';
+  test('addTodo', () => {
+    addTodo(arr, todoStr)
+    expect(arr).toHaveLength(1);
+    expect(arr[0].description).toBe('test');
+  })
+})
