e4393acdc4682f23ee6b431a524fcd54765f1767
Khalil <khalilnasri92@gmail.com>
2021-12-09 14:56:38 +0100

    update code with required changes

--- src/updateTodo.js
+++ src/updateTodo.js
@@ -1,10 +1,7 @@
 import localStorageMock from './__mocks__/localStorageMock.js';
 
 export default (id, arr, str) => {
-  // const id = Number(e.target.getAttribute('data-index'));
-  // const data = JSON.parse(localStorage.getItem('todoList'));
   const todoObj = arr.find((x) => x.index === id);
-
   todoObj.description = str;
   localStorageMock.setItem('todoList', JSON.stringify(todoObj));
 };
