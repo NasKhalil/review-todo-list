f8f32207594160a440669056e407a7f78d4c276a
Khalil <khalilnasri92@gmail.com>
2021-12-09 14:41:57 +0100

    fix linters errors

--- src/addTodo.test.js
+++ src/addTodo.test.js
@@ -13,27 +13,26 @@ describe('test add function', () => {
     expect(arr).toHaveLength(1);
     expect(arr[0].description).toBe('test');
   });
-// deleteTodo test
+  // deleteTodo test
   test('deleteTodo', () => {
     const todo = arr[0];
     arr = deleteTodo(todo, arr);
     expect(arr).toHaveLength(0);
   });
-// updateTodo test
+  // updateTodo test
   test(' updateTodo', () => {
-    arr.push({completed:false,description:'hello world',index:1});
-    updateTodo(1,arr,'hey')
+    arr.push({ completed:false, description:'hello world', index:1 });
+    updateTodo(1, arr, 'hey');
     expect(arr[0].description).toBe('hey');
   });
-// completed status test
+  // completed status test
   test('update completed status', () => {
     checkBoxEvent(arr);
     expect(arr[0].completed).toBe(true);
-  })
+  });
 
   test('clear all completed task', () => {
-    
     arr = clearAllCompleted(arr);
     expect(arr).toHaveLength(0);
-  })
+  });
 });
